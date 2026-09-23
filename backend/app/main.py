552bb11af074b038c64b4090313830e3a4ef404b
Neknarayan <neknarayanlodhi63@gmail.com>
2026-09-24 00:58:27 +0530

    feat: update CORS settings to allow all methods and headers

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,8 +26,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allow_headers=["Authorization", "Content-Type", "Accept"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Mount Routers
